957265c5566398737a50abdf62b58750ec71384d
bevanyudira <bevanydr404@gmail.com>
2024-12-03 14:22:19 +0700

    foreach pada file blade transaksidetail

--- resources/views/transaksidetail/index.blade.php
+++ resources/views/transaksidetail/index.blade.php
@@ -9,30 +9,28 @@
         <div class="card-body">
             <table class="table table-bordered datatable">
                 <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Tanggal Pembelian</th>
-                    <th>Produk</th>
-                    <th>Harga Satuan</th>
-                    <th>Jumlah</th>
-                    <th>Subtotal</th>
-                </tr>
+                    <tr>
+                        <th>No</th>
+                        <th>Tanggal Pembelian</th>
+                        <th>Produk</th>
+                        <th>Harga Satuan</th>
+                        <th>Jumlah</th>
+                        <th>Subtotal</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ( as $index => $data)
-                    <tr>
-                        <td>{{ $index+1 }}</td>
-                        <td>{{ \Carbon\Carbon::parse($data->transaksi->tanggal)->format('d/m/Y') }}</td>
-                        <td>{{ $data->nama_produk }}</td>
-                        <td class="text-end">{{ number_format($data->hargasatuan, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->junlah, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->subt0tal, 0, '.', '.') }}</td>
-                    </tr>
-                @endforeach
+                    @foreach ($transaksidetail as $index => $data)
+                        <tr>
+                            <td>{{ $index+1 }}</td>
+                            <td>{{ \Carbon\Carbon::parse($data->transaksi->tanggal)->format('d/m/Y') }}</td>
+                            <td>{{ $data->nama_produk }}</td>
+                            <td class="text-end">{{ number_format($data->harga_satuan, 0, '.', '.') }}</td>
+                            <td class="text-end">{{ number_format($data->jumlah, 0, '.', '.') }}</td>
+                            <td class="text-end">{{ number_format($data->subtotal, 0, '.', '.') }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
-
-    @endsection
-
+@endsection
